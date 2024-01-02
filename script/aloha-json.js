24fef2b9e88e503fc2749a4377493b18fbe0168b
Joee <32849562+Joee-D@users.noreply.github.com>
2024-01-02 17:37:41 +0800

    Create aloha-json.js

---
+++ script/aloha-json.js
@@ -0,0 +1,39 @@
+const url = $request.url;
+const method = $request.method;
+const notifyTitle = "aloha-json";
+console.log(`aloha-json-2024.01.02`);
+if (!$response.body) {
+    // 有undefined的情况
+    console.log(`$response.body为undefined:${url}`);
+    $done({});
+}
+if (method !== "GET") {
+    $notification.post(notifyTitle, "method错误:", method);
+}
+let body = JSON.parse($response.body);
+
+
+if (url.includes("v1/profile_info")) {
+    console.log('会员资格修改');
+    body.profile.is_premium = true;
+    console.log('成功');
+} else if (url.includes("v1/blockedvideo")) {
+    console.log('去除视频下载屏蔽');
+    body.videos = [];
+    console.log('成功');
+} else if (url.includes("v1/adblock_whitelist")) {
+    console.log('去除广告白名单');
+    body = [];
+    console.log('成功');
+} else if (url.includes("v1/speed_dial_tiles")) {
+    console.log('去除首页悬浮广告');
+    body.modal = false;
+    console.log('成功');
+} else {
+    $notification.post(notifyTitle, "路径匹配错误:", url);
+}
+
+body = JSON.stringify(body);
+$done({
+    body
+});
