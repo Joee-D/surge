74cecfadf3f853de08f59c5295aecd98e23b2426
Joee <32849562+Joee-D@users.noreply.github.com>
2024-01-05 09:55:46 +0800

    Update timecard.js

--- script/timecard.js
+++ script/timecard.js
@@ -775,7 +775,7 @@ function title_random(num){
 $done({
 //title:title_random(tnumcount(Number(nowlist))),
 //icon:icon_now(tnumcount(Number(nowlist))),
-title:nowlunar,
+title:nowsolar+''+nowlunar,
 icon:"calendar",
 content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+","+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
 })
