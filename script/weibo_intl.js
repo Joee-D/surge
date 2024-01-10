84e27034b2aacb1c84f2e43d12b73503bf7ec026
Joee <32849562+Joee-D@users.noreply.github.com>
2024-01-10 10:13:38 +0800

    Create weibo_intl.js

---
+++ script/weibo_intl.js
@@ -0,0 +1,13 @@
+const url = $request.url;
+let body = JSON.parse($response.body);
+if (url.includes("friends_timeline") || url.includes("friends/timeline")) {
+  body.advertises=[];
+  body.ad=[];
+} else if (url.includes("ad/weibointl") {
+  body.data=[];
+}
+
+body = JSON.stringify(body);
+$done({
+    body
+});
