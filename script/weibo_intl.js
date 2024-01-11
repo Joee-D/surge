066c51192437aaa10faca79cf6fe183eda3c91ca
Joee <32849562+Joee-D@users.noreply.github.com>
2024-01-11 16:41:36 +0800

    Update weibo_intl.js

--- script/weibo_intl.js
+++ script/weibo_intl.js
@@ -3,8 +3,10 @@ let body = JSON.parse($response.body);
 if (url.includes("friends/timeline")) {
   body.advertises=[];
   body.ad=[];
-} else if (url.includes("ad/weibointl") || url.includes("get_coopen_ads")) {
+} else if (url.includes("ad/weibointl")) {
   body.data=[];
+} else if (url.includes("get_coopen_ads")) {
+  body.data={};
 }
 
 body = JSON.stringify(body);
