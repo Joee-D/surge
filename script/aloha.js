5b3a917e28fd04ba60d9f80b30ae3daedf6c4bfc
Joee <32849562+Joee-D@users.noreply.github.com>
2024-01-15 10:37:20 +0800

    Update aloha.js

--- script/aloha.js
+++ script/aloha.js
@@ -1,15 +1,9 @@
 const url = $request.url;
 let body = JSON.parse($response.body);
 
-if (url.includes("v1/profile_info")) {
-    body.profile.is_premium = true;
-    body.profile.end_of_premium = 3818419199,
-    body.profile._end_of_premium = "2090-12-31 23:59:59"
-} else if (url.includes("v1/blockedvideo")) {
-    body.videos = [];
-} else if (url.includes("v1/speed_dial_tiles")) {
-    body.modal = false;
-}
+body.profile.is_premium = true;
+body.profile.end_of_premium = 3818419199,
+body.profile._end_of_premium = "2090-12-31 23:59:59"
 
 body = JSON.stringify(body);
 $done({
