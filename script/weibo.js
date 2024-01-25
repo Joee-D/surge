886e2f3e0345e88b6d32c08c3f339209e8d47bec
Joee <32849562+Joee-D@users.noreply.github.com>
2024-01-25 11:11:03 +0800

    Update weibo.js

--- script/weibo.js
+++ script/weibo.js
@@ -2,14 +2,20 @@ const url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if (obj.items && obj.items.length > 0) {
-  let i = obj.items.length;
-  while(i--) {
-    if(obj.items[i].data && obj.items[i].data.mblogtype && obj.items[i].data.mblogtype == 1) {
-      obj.items.splice(i, 1);
-    }
-    // obj.items[i].data.ad_state == 1
-  } 
+if (url.includes("container_timeline") || url.includes("searchall")) {
+  if (obj.items && obj.items.length > 0) {
+    let i = obj.items.length;
+    while(i--) {
+      if(obj.items[i].data && obj.items[i].data.mblogtype && obj.items[i].data.mblogtype == 1) {
+        obj.items.splice(i, 1);
+      }
+    } 
+  }
+} else if (url.includes("extend")) {
+  if (obj.head_cards) {
+    delete obj.head_cards;
+  }
 }
+
 body = JSON.stringify(obj);
 $done({ body });
