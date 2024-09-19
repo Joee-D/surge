977a8360549126d73a5c6610a4fcef6333e13798
Joee <32849562+Joee-D@users.noreply.github.com>
2024-09-19 20:37:10 +0800

    Update aloha.js

--- script/aloha.js
+++ script/aloha.js
@@ -2,8 +2,10 @@ const url = $request.url;
 let body = JSON.parse($response.body);
 
 body.profile.is_premium = true;
-body.profile.end_of_premium = 3818419199,
-body.profile._end_of_premium = "2090-12-31 23:59:59"
+body.profile.has_active_paid_subscription = true;
+body.profile.end_of_premium = 3818419199;
+body.profile._end_of_premium = "2090-12-31 23:59:59";
+body.profile.active_premium_sources.purchase = true;
 
 body = JSON.stringify(body);
 $done({
