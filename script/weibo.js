543d10f1385bbf6feede6c4d2f55e9f829355fdd
Joee <32849562+Joee-D@users.noreply.github.com>
2024-09-19 20:34:15 +0800

    Update weibo.js

--- script/weibo.js
+++ script/weibo.js
@@ -2,7 +2,7 @@ const url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if (url.includes("container_timeline") || url.includes("finder")) {
+if (url.includes("container_timeline")) {
   if (obj.items && obj.items.length > 0) {
     let i = obj.items.length;
     while(i--) {
