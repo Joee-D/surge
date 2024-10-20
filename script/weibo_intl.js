54f7e25e3ebba1f64c0863dbdd586798d9c0d225
Joee <32849562+Joee-D@users.noreply.github.com>
2024-10-20 16:32:44 +0800

    Update weibo_intl.js

--- script/weibo_intl.js
+++ script/weibo_intl.js
@@ -1,31 +1,31 @@
 let url = $request.url;
 let body = $response.body;
-let e = JSON.parse(body);
+let obj = JSON.parse(body);
 if (url.includes("a=trends")) {
-	if (e.data?.order) {
-    e.data.order = ["search_topic"];
+  if (obj.data?.order) {
+    obj.data.order = ["search_topic"];
   }
 } else if (url.includes("a=open_app")) {
-  if (e.data) {
-    e.data.uve_ad_scene = "";
-    e.data.vip_info.tips = {};
-    e.data.vip_title_ad = "";
-    e.data.close_ad_setting = {};
-    e.data.background_preview = "";
-    e.data.detail_banner_ad = [];
+  if (obj.data) {
+    obj.data.uve_ad_scene = "";
+    obj.data.vip_info.tips = {};
+    obj.data.vip_title_ad = "";
+    obj.data.close_ad_setting = {};
+    obj.data.background_preview = "";
+    obj.data.detail_banner_ad = [];
   }
 } else if (url.includes("a=get_coopen_ads")) {
-  if (e.data) {
-    e.data.ad_list = [];
-    e.data.gdt_video_ad_ios = [];
-    e.data.display_ad = 0;
-    e.data.ad_ios_id = null;
-    e.data.app_ad_ios_id = null;
-    e.data.reserve_ad_ios_id = "";
-    e.data.reserve_app_ad_ios_id = "";
-    e.data.ad_duration = 6e6;
-    e.data.ad_cd_interval = 6e6;
-    e.data.pic_ad = [];
+  if (obj.data) {
+    obj.data.ad_list = [];
+    obj.data.gdt_video_ad_ios = [];
+    obj.data.display_ad = 0;
+    obj.data.ad_ios_id = null;
+    obj.data.app_ad_ios_id = null;
+    obj.data.reserve_ad_ios_id = "";
+    obj.data.reserve_app_ad_ios_id = "";
+    obj.data.ad_duration = 6e6;
+    obj.data.ad_cd_interval = 6e6;
+    obj.data.pic_ad = [];
   }
 }
-$done({ body: JSON.stringify(e) });
+$done({ body: JSON.stringify(obj) });
