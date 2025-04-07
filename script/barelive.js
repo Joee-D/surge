9008427026b663d239291d4303134bcc63710d58
Joee <32849562+Joee-D@users.noreply.github.com>
2025-04-07 17:27:15 +0800

    Create barelive.js

---
+++ script/barelive.js
@@ -0,0 +1,18 @@
+const url = $request.url;
+let body = $response.body;
+let obj = JSON.parse(body);
+
+if (url.includes("v1/lives")) {
+  if (obj.is_lock) {
+    obj.is_lock = false;
+  } else if (obj.watermark_enabled) {
+    obj.watermark_enabled = false;
+  }
+} else if (url.includes("v3/profile")) {
+  if (obj.premium) {
+    obj.premium = true;
+  }
+}
+
+body = JSON.stringify(obj);
+$done({ body });
