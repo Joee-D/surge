736cc68515f62134464f130a8beb9702e3ef745e
Joee <32849562+Joee-D@users.noreply.github.com>
2025-04-07 19:09:28 +0800

    Update barelive.js

--- script/barelive.js
+++ script/barelive.js
@@ -3,9 +3,16 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 if (url.includes("v1/lives")) {
+  if (obj.lock_display_time) {
+    obj.lock_display_time = null;
+  }
+  if (obj.lock_session) {
+    obj.lock_session = null;
+  }
   if (obj.is_lock) {
     obj.is_lock = false;
-  } else if (obj.watermark_enabled) {
+  }
+  if (obj.watermark_enabled) {
     obj.watermark_enabled = false;
   }
 } else if (url.includes("v3/profile")) {
